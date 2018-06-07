086752ff3d7672216291593c5d85f1d2f7ab24f6
Zehan Sunesara <usazehan@gmail.com>
2018-06-07 12:44:42 -0500

    Add files via upload
    Refactored as well as added new functionalities

--- p2pChat/chat.py
+++ p2pChat/chat.py
@@ -1,14 +1,18 @@
 import socket
 import threading
 import sys
+import time
+from random import randint
 
 class Server:
     connections = []
     peers = []
     def __init__(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #got this from the socket library
         sock.bind(('0.0.0.0', 10000))
         sock.listen(1)
+        print("Server running ...")
         while True:
             c, a = sock.accept()
             cThread = threading.Thread(target=self.handler, args=(c,a))
@@ -33,6 +37,11 @@ class Server:
                 break
     def sendPeers(self):
         p = ""
+        for peer in self.peers:
+            p = p + peer + ","
+
+        for connection in self.connections:
+            connection.send(b'\x11' + bytes(p, "utf-8"))
 
 class Client:
     def sendMsg(self):
@@ -40,6 +49,7 @@ class Client:
             sock.send(bytes(input(""), 'utf-8'))
     def __init__(self, address):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #got this from the socket library
         sock.connect((address, 10000))
 
         iThread = threading.Thread(target=self.sendMsg, args= (sock, ))
@@ -50,9 +60,35 @@ class Client:
             data = sock.recv(1024)
             if not data:
                 break
-            print(str(data, 'utf-8'))
+            if data[0:1] == b'\x11':
+                #print("got peers")
+                self.updatedPeers(data[1:])
+            else:
+                print(str(data, 'utf-8'))
 
-if(len(sys.argv) > 1):
-    client = Client()
-else:
-    server = Server()
+    def updatedPeers(self, peerData):
+        p2p.peers = str(peerData, "utf-8").split(",")[:-1]
+
+class p2p:
+    peers = ['127.0.0.1']
+
+while True:
+    try:
+        print("Trying to connect ...")
+        time.sleep(randint(1, 5))
+        for peer in p2p.peers:
+            try:
+                client = Client(peer)
+            except KeyboardInterrupt:
+                sys.exit(0)
+            except:
+                pass
+            if randint(1, 20) ==1:
+                try:
+                    server = Server()
+                except KeyboardInterrupt:
+                    sys.exit(0)
+                except:
+                    print("Couldn't start the server")
+    except KeyboardInterrupt:
+        sys.exit(0)
